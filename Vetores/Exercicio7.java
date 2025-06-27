a5a313005f6c8591384736c12d1bc28e9107f283
Mabss777 <mabssdugrauu07@gmail.com>
2025-06-27 09:44:48 -0300

    Exercicio 7 incompleto!

---
+++ Vetores/Exercicio7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ 
+public class Exercicio6 {
+ 
+    public static void main(String[] args) {
+ 
+      int[] numRecebido = new int[6];
+
+      for(int i=0; i<6; i++){
+
+            System.out.println("Digite um numero inteiro: ");
+            numRecebido[i] = scanner.nextInt();
+        }
+    }
+}
