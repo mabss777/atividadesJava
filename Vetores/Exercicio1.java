e53ae7fa51bed76015a9d45f59cfce1a40e41406
Mabss777 <mabssdugrauu07@gmail.com>
2025-06-27 08:27:34 -0300

    Exercicio 1 feito!

---
+++ Vetores/Exercicio1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Exercicio1 {
+    public static void main(String[] args){
+        int[] numDigitado = new int[6];
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i=0; i<6; i++){
+
+            System.out.println("Digite um numero inteiro: ");
+            numDigitado[i] = scanner.nextInt();
+        }
+
+        for(int i = 0; i <6; i++){
+            System.out.println("Valor "+(i+1)+": "+numDigitado[i]);
+        }
+
+        
+
+
+
+    }
+}
