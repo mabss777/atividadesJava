f6da5c0e5837e7db7479bd6d0890b3759b8aa52e
Mabss777 <mabssdugrauu07@gmail.com>
2025-06-27 09:02:33 -0300

    Exercicio 5 feito!

---
+++ Vetores/Exercicio5.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+ 
+public class Exercicio5 {
+ 
+    public static void main(String[] args) {
+ 
+        String[] alunos = new String[15];
+        double[] notas = new double[15];  // array para armazenar as notas de cada aluno
+ 
+        Scanner scanner = new Scanner(System.in);
+ 
+        for (int i = 0; i < 15; i++) {
+ 
+            System.out.println("Digite o nome do aluno " + (i + 1) + ":");
+            alunos[i] = scanner.nextLine();
+ 
+            System.out.println("Digite a nota do aluno " + alunos[i] + ":");
+            notas[i] = scanner.nextDouble();
+ 
+            scanner.nextLine(); // limpa buffer
+        }
+ 
+        // Calcula média geral
+        double soma = 0;
+        for (int i = 0; i < 15; i++) {
+            soma += notas[i];
+        }
+        double mediaGeral = soma / 15;
+ 
+        System.out.println("Média geral da turma: " + mediaGeral);
+ 
+        scanner.close();
+    }
+}
