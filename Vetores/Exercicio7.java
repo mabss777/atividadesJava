3202236b223ec49aa8a4f56ea45744be0b117e4d
Mabss777 <mabssdugrauu07@gmail.com>
2025-06-27 11:17:01 -0300

    Exercicio 7 feito!

--- Vetores/Exercicio7.java
+++ Vetores/Exercicio7.java
@@ -1,15 +1,41 @@
 import java.util.Scanner;
- 
-public class Exercicio6 {
- 
+
+public class Exercicio7 {
+
     public static void main(String[] args) {
- 
-      int[] numRecebido = new int[6];
 
-      for(int i=0; i<6; i++){
+        int[] numeros = new int[6];
+        int somaPares = 0;
+        int qtdImpares = 0;
 
-            System.out.println("Digite um numero inteiro: ");
-            numRecebido[i] = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+
+        // Entrada dos numeros
+        for (int i = 0; i < 6; i++) {
+            System.out.print("Digite o " + (i + 1) + "° numero inteiro: ");
+            numeros[i] = scanner.nextInt();
         }
+
+        System.out.println("\nNumeros pares digitados:");
+        for (int num : numeros) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+                somaPares += num;
+            }
+        }
+
+        System.out.println("\nSoma dos numeros pares: " + somaPares);
+
+        System.out.println("\nNumeros impares digitados:");
+        for (int num : numeros) {
+            if (num % 2 != 0) {
+                System.out.println(num);
+                qtdImpares++;
+            }
+        }
+
+        System.out.println("\nQuantidade de numeros impares: " + qtdImpares);
+
+   
     }
 }
