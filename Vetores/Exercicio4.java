ca1d5b8414ea1c6f329ca724eb8704b091b69d5b
Mabss777 <mabssdugrauu07@gmail.com>
2025-06-30 08:42:32 -0300

    Exercício 4 feito!

--- Vetores/Exercicio4.java
+++ Vetores/Exercicio4.java
@@ -4,13 +4,13 @@ public class Exercicio4 {
  
     public static void main (String[] args){
  
-    double[]valores= new double [6];
+    int[]valores= new int [6];
  
     for(int i=0; i<6; i++){
  
          int numeroRandomico = (int)(Math.random()* 101);
         valores[i]=numeroRandomico;
-        System.out.println("Vetor na ordem normal "+(i+1)+":"+numeroRandomico);
+        System.out.println("Vetor na ordem normal "+(i+1)+": "+numeroRandomico);
  
         }
  
