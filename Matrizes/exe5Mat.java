2fdb78382ec5abb301804c50b172fb3823eb47be
Mabss777 <mabssdugrauu07@gmail.com>
2025-07-01 08:30:34 -0300

    Exercício 5 feito!

--- Matrizes/exe5Mat.java
+++ Matrizes/exe5Mat.java
@@ -2,56 +2,48 @@ import java.util.Random;
 
 public class exe5Mat {
     public static void main(String[] args) {
-        Random numRandomico = new Random();
         int[][] quadradoMagico = new int[3][3];
-        boolean magico;
+        Random aleatorio = new Random();
 
-        do {
-            int[] somaLinhas = new int[3];
-            int[] somaColunas = new int[3];
-            int diagonalPrincipal = 0;
-            int diagonalSecundaria = 0;
-            magico = true;
-
-            // Gerar nova matriz aleatória
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    quadradoMagico[i][j] = numRandomico.nextInt(20) + 1;
-                    System.out.print(quadradoMagico[i][j] + "\t");
-                }
-                System.out.println();
+        System.out.println("Matriz 3x3 com numeros aleatorios:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                quadradoMagico[i][j] = aleatorio.nextInt(10) + 1;
+                System.out.print(quadradoMagico[i][j] + "\t");
             }
+            System.out.println();
+        }
 
-            // Calcular somas
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    somaLinhas[i] += quadradoMagico[i][j];
-                    somaColunas[j] += quadradoMagico[i][j];
-                    if (i == j) diagonalPrincipal += quadradoMagico[i][j];
-                    if (i + j == 2) diagonalSecundaria += quadradoMagico[i][j];
-                }
+        int[] somaLinhas = new int[3];
+        int[] somaColunas = new int[3];
+        int diagonalPrincipal = 0;
+        int diagonalSecundaria = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                somaLinhas[i] += quadradoMagico[i][j];
+                somaColunas[j] += quadradoMagico[i][j];
+                if (i == j)
+                    diagonalPrincipal += quadradoMagico[i][j];
+                if (i + j == 2)
+                    diagonalSecundaria += quadradoMagico[i][j];
             }
+        }
 
-            int valorMagico = somaLinhas[0];
+        boolean ehMagico = true;
+        int valorMagico = somaLinhas[0];
 
-            for (int i = 0; i < 3; i++) {
-                if (somaLinhas[i] != valorMagico || somaColunas[i] != valorMagico) {
-                    magico = false;
-                    break;
-                }
+        for (int i = 0; i < 3; i++) {
+            if (somaLinhas[i] != valorMagico || somaColunas[i] != valorMagico) {
+                ehMagico = false;
+                break;
             }
+        }
 
-            if (diagonalPrincipal != valorMagico || diagonalSecundaria != valorMagico) {
-                magico = false;
-            }
+        if (diagonalPrincipal != valorMagico || diagonalSecundaria != valorMagico) {
+            ehMagico = false;
+        }
 
-            System.out.println("\nResultado:");
-            if (magico) {
-                System.out.println(" EH UM QUADRADO MAGICO!!! \n");
-            } else {
-                System.out.println(" Nao eh um quadrado magico... tentando de novo!\n");
-            }
-
-        } while (!magico);
+        System.out.println(ehMagico ? "\nE um quadrado magico!" : "\nNao e um quadrado magico.");
     }
 }
